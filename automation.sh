1abe54dba3ffa70bac21940ca27f9f72009b17e5
vivekrajdev <37079673+vivekrajdev@users.noreply.github.com>
2022-04-13 17:49:39 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -38,3 +38,22 @@ sudo apt-get install awscli -y
 aws s3 \
 cp /tmp/${name}-httpd-logs-${timestp}.tar \
 s3://${s3bucket}/${name}-httpd-logs-${timestp}.tar
+
+if [ -e /var/www/html/inventory.html ]
+then
+        echo "Inventory exists"
+else
+        touch /var/www/html/inventory.html
+        echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Size</b>" >> /var/www/html/inventory.html
+fi
+
+echo "<br>httpd-logs &nbsp;&nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp;&nbsp; `du -h /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}'`" >> /var/www/html/inventory.html
+
+if [ -e /etc/cron.d/automation ]
+then
+        echo "Cron job exists"
+else
+        touch /etc/cron.d/automation
+        echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+        echo "Cron job added"
+fi
